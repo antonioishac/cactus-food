0810f6360ed0d958c7e92b8f294300ab3a6acb7f
antonioishac <antonioishac@gmail.com>
2019-11-30 16:36:27 -0300

    Iniciando o treinamento especialista spring

---
+++ src/test/java/br/com/cactus/food/CactusfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cactus.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CactusfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
